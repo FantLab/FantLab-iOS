40d0c7c571117fef0ff4dfcdb270630467f62169
Geor Kasapidi <geor.kasapidi@icloud.com>
2018-11-10 12:23:01 +0300

    start project

---
+++ Sources/FantLab-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FantLab-iOS
+//
+//  Created by Geor Kasapidi on 10/11/2018.
+//  Copyright © 2018 FantLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Sources/FantLab-iOSTests/FantLab_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  FantLab_iOSTests.swift
+//  FantLab-iOSTests
+//
+//  Created by Geor Kasapidi on 10/11/2018.
+//  Copyright © 2018 FantLab. All rights reserved.
+//
+
+import XCTest
+@testable import FantLab_iOS
+
+class FantLab_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
