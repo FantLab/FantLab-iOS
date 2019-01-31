5fb6dd4eb7fe5030d0f52826b0f8b731891934ea
Georgy Kasapidi <geor.kasapidi@icloud.com>
2019-01-31 18:43:01 +0300

    fix title size

--- Sources/Modules/Style/TitleFontSizeRule.swift
+++ Sources/Modules/Style/TitleFontSizeRule.swift
@@ -7,12 +7,10 @@ public final class TitleFontSizeRule {
     public static func fontSizeFor(length: Int) -> CGFloat {
         if length < 10 {
             return 28
-        } else if length < 30 {
+        } else if length < 40 {
             return 24
-        } else if length < 50 {
-            return 20
         } else {
-            return 18
+            return 20
         }
     }
 }
