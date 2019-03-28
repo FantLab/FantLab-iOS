f73f8be2677e6a3a5c9c68ba2863c0b7df84f335
Geor Kasapidi <geor.kasapidi@icloud.com>
2019-03-28 14:36:46 +0300

    remake main project with xcode 10.2

--- Sources/FantLabTests/FantLabTests.swift
+++ Sources/FantLabTests/FantLabTests.swift
@@ -6,127 +6,127 @@ import FLText
 class TextParserTests: XCTestCase {
     func testEmptyString() {
         let text = FLStringPreview(string: "")
-
+        
         XCTAssert(text.value.isEmpty)
     }
-
+    
     func testOpenTag() {
         let text = FLStringPreview(string: "[b]")
-
+        
         XCTAssert(text.value.isEmpty)
     }
-
+    
     func testSingleBBTag() {
         let text = FLStringPreview(string: "[b]lorem ipsum[/b]")
-
+        
         XCTAssert(text.value == "lorem ipsum")
     }
-
+    
     func testMultipleBBTags() {
         let text = FLStringPreview(string: "x y [b][u]z[/u][/b] a [i]b[/i] [p]c[/p] d e")
-
+        
         XCTAssert(text.value == "x y z a b c d e")
     }
-
+    
     func testBBTagWithParam() {
         let text = FLStringPreview(string: "some [person=1]person[/person]")
-
+        
         XCTAssert(text.value == "some person")
     }
-
+    
     func testURLInBBTag() {
         let text = FLStringPreview(string: "some [url=https://google.com?q=fantlab]url[/url]")
-
+        
         XCTAssert(text.value == "some url")
     }
-
+    
     func testSingleHTMLTag() {
         let text = FLStringPreview(string: "<b>lorem ipsum</b>")
-
+        
         XCTAssert(text.value == "lorem ipsum")
     }
-
+    
     func testHTMLLink() {
         let text = FLStringPreview(string: "<a href=\"link\">lorem ipsum</a>")
-
+        
         XCTAssert(text.value == "lorem ipsum")
     }
-
+    
     func testHTMLLink2() {
         let text = FLStringPreview(string: "<a href=\"link\" href='link2' href=“link3“>lorem ipsum</a>")
-
+        
         XCTAssert(text.value == "lorem ipsum")
     }
-
+    
     func testHTMLAndBBTags() {
         let text = FLStringPreview(string: "<a href=\"link\">[b]lorem ipsum[/b]</a>")
-
+        
         XCTAssert(text.value == "lorem ipsum")
     }
-
+    
     func testSingleEmoticon() {
         let text = FLStringPreview(string: "lorem :smile: ipsum")
-
+        
         XCTAssert(text.value == "lorem 😊 ipsum")
     }
-
+    
     func testSingleUnknownEmoticon() {
         let text = FLStringPreview(string: "lorem :smil: ipsum")
-
+        
         XCTAssert(text.value == "lorem :smil: ipsum")
     }
-
+    
     func testEmoticons() {
         let text = FLStringPreview(string: "lorem :smile::help::hmm: ipsum")
-
+        
         XCTAssert(text.value == "lorem 😊🆘🤔 ipsum")
     }
-
+    
     func testHybridText() {
         let text = FLStringPreview(string: "lorem <a href='link'>ipsum</a> dolor [b]sit[/b] amet, consectetur adipiscing <b><i>:smile:</i></b>:foo")
-
+        
         XCTAssert(text.value == "lorem ipsum dolor sit amet, consectetur adipiscing 😊:foo")
     }
-
+    
     func testHiddenTag() {
         let text = FLStringPreview(string: "lorem [h]ipsum <b>dolor</b> sit[/h] amet")
-
+        
         XCTAssert(text.value == "lorem ... amet")
     }
-
+    
     func testSingleBBTags1() {
         let text = FLStringPreview(string: "list [LIST][*]1st [*]2nd [*]3rd ...[/LIST] end")
-
+        
         XCTAssert(text.value == "list 1st 2nd 3rd ... end")
     }
-
+    
     func testSingleBBTags2() {
         let text = FLStringPreview(string: "my [IMG]link[/IMG] and my [VIDEO=video_link] !!!")
-
+        
         XCTAssert(text.value == "my ... and my ... !!!")
     }
-
+    
     func testSingleHTMLTags() {
         let text = FLStringPreview(string: "my <img src'link'/> and <br> !!!")
-
+        
         XCTAssert(text.value == "my ... and \n !!!")
     }
-
+    
     func testHtmlEntities() {
         let text = FLStringPreview(string: "Переводные издания Новинки Айлингтон Джеймс &laquo;Тень ушедшего&raquo;Алендер")
-
+        
         XCTAssert(text.value == "Переводные издания Новинки Айлингтон Джеймс «Тень ушедшего»Алендер")
     }
-
+    
     func testHtmlEntitiesAndSingleAnd() {
         let text = FLStringPreview(string: "Переводные издания Новинки Айлингтон Джеймс &laquo;Тень ушедшего&raquo;&Алендер")
-
+        
         XCTAssert(text.value == "Переводные издания Новинки Айлингтон Джеймс «Тень ушедшего»&Алендер")
     }
-
+    
     func testHtmlUnknownEntity() {
         let text = FLStringPreview(string: "Переводные издания Новинки Айлингтон Джеймс &laqo;Тень ушедшего&raqo;&Алендер")
-
+        
         XCTAssert(text.value == "Переводные издания Новинки Айлингтон Джеймс &laqo;Тень ушедшего&raqo;&Алендер")
     }
 }
