43bbdd55e4301789899398fec732366e4e136ce7
Geor Kasapidi <geor.kasapidi@icloud.com>
2019-04-03 08:58:51 +0300

    update allkit

--- Sources/Modules/ContentBuilders/RemoveActionLayoutSpec.swift
+++ Sources/Modules/ContentBuilders/RemoveActionLayoutSpec.swift
@@ -1,21 +1,27 @@
 import Foundation
 import UIKit
 import ALLKit
+import yoga
 
 public final class RemoveActionLayoutSpec: LayoutSpec {
     public override func makeNodeWith(sizeConstraints: SizeConstraints) -> LayoutNode {
         let icon = UIImage(named: "trash")
-
-        let iconNode = LayoutNode(sizeProvider: icon?.size) { (view: UIImageView, _) in
+        let size = icon?.size ?? .zero
+        
+        let iconNode = LayoutNode(config: { node in
+            node.width = YGValue(size.width)
+            node.height = YGValue(size.height)
+        }) { (view: UIImageView, _) in
+            view.contentMode = .scaleAspectFill
             view.image = icon?.withRenderingMode(.alwaysTemplate)
             view.tintColor = UIColor.white
         }
-
+        
         let contentNode = LayoutNode(children: [iconNode], config: { node in
             node.alignItems = .center
             node.justifyContent = .center
         })
-
+        
         return contentNode
     }
 }
